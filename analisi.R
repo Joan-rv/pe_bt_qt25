ab9de9519723ff768eec7acba56fb0b0fe117590
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-12-15 12:48:38 +0100

    Millorar models

--- analisi.R
+++ analisi.R
@@ -43,7 +43,6 @@ dades_aparellades <- paired(x, y)
 plot(dades_aparellades,type="BA")
 
 
-# T_dfs ~ algorithm
 M1_dfs <- lm(log(dfs_time) ~ algorithm, data=dades)
 summary(M1_dfs)
 # Premises model:
@@ -66,6 +65,13 @@ plot(M1_bfs,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
 hist(rstandard(M1_bfs),font.main=1)     # Histograma dels residus estandaritzats
 plot(rstandard(M1_bfs),type="l")	      # Ordre dels residus estandaritzats
 
+M1 <- lm(bfs_time - dfs_time ~ algorithm, data=dades)
+summary(M1)
+par(mfrow=c(2,2))
+plot(M1,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
+hist(rstandard(M1),font.main=1)     # Histograma dels residus estandaritzats
+plot(rstandard(M1),type="l")	      # Ordre dels residus estandaritzats
+
 M2_dfs <- lm(log(dfs_time) ~ num_vertices + num_edges + cpu_model, data=d_numcc)
 summary(M2_dfs)
 # normalitat residus: sí, però al final no
@@ -87,3 +93,32 @@ par(mfrow=c(2,2))
 plot(M2_bfs,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
 hist(rstandard(M2_bfs),font.main=1)     # Histograma dels residus estandaritzats
 plot(rstandard(M2_bfs),type="l")	      # Ordre dels residus estandaritzats
+
+M3_dfs <- lm(log(dfs_time) ~ num_vertices + num_edges + cpu_model, data=d_numcc)
+summary(M3_dfs)
+# normalitat residus: sí, però al final no
+# homoscedasticitat: sí
+# linealitat: sí
+# independència: sí
+par(mfrow=c(2,2))
+plot(M3_dfs,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
+hist(rstandard(M3_dfs),font.main=1)     # Histograma dels residus estandaritzats
+plot(rstandard(M3_dfs),type="l")	      # Ordre dels residus estandaritzats
+
+M3_bfs <- lm(log(bfs_time) ~ num_vertices + num_edges + cpu_model, data=d_numcc)
+summary(M3_bfs)
+# normalitat residus: sí, però al final no
+# homoscedasticitat: sí
+# linealitat: sí
+# independència: sí
+par(mfrow=c(2,2))
+plot(M3_bfs,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
+hist(rstandard(M3_bfs),font.main=1)     # Histograma dels residus estandaritzats
+plot(rstandard(M3_bfs),type="l")	      # Ordre dels residus estandaritzats
+
+M4 <- lm(dfs_time - bfs_time ~ algorithm, data=dades)
+summary(M4)
+par(mfrow=c(2,2))
+plot(M4,c(2,1))          		        # QQ-Norm i Standard Residuals vs. Fitted
+hist(rstandard(M4),font.main=1)     # Histograma dels residus estandaritzats
+plot(rstandard(M4),type="l")	      # Ordre dels residus estandaritzats
